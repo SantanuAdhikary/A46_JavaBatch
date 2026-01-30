5cea86c35597ec6f8cf9eed6923e55443de5aef0
SantanuAdhikary <santanuadhikary13@gmail.com>
2026-01-30 11:56:12 +0530

    prime number added

---
+++ loop/PrimeNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class  PrimeNumber
+{
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+		System.out.print("enter one number : ");
+		int n = sc.nextInt();
+		
+		int count = 0 ;
+		
+		for(int i=1 ; i<=n ;i++)
+		{
+			if(n % i == 0)
+				count++;				
+		}
+		
+		if(count == 2 )
+			System.out.println("it is prime number");
+		else
+			System.out.println("it is not prime number");
+	}
+}
