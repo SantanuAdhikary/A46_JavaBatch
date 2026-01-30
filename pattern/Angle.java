12dd01826c2d1d154d7e2a2479596adac50f0e7a
SantanuAdhikary <santanuadhikary13@gmail.com>
2026-01-30 15:28:54 +0530

    all patterns code added

---
+++ pattern/Angle.java
@@ -0,0 +1,29 @@
+class  Angle1
+{
+	public static void main(String[] args) throws Exception
+	{
+		
+		int n = 9 ; 
+		
+		for(int i=0 ; i<n ; i++)
+		{
+			for(int j=0 ; j<n ;j++)
+			{
+				if(i>=j)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+				
+				Thread.sleep(50);
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+/*
+
+
+
+*/
---
+++ pattern/Box2.java
@@ -0,0 +1,34 @@
+class Box2 
+{
+	public static void main(String[] args) throws Exception
+	{
+		
+		int n = 5 ;
+		
+		for(int i=0 ; i<n ; i++)
+		{
+			for(int j=0 ; j<n ;j++)
+			{
+				if(i==0 || i==n-1 || j==0 || j==n-1)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+				
+				Thread.sleep(50);
+			}
+			System.out.print("   ");
+			for(int j=0 ; j<n ;j++)
+			{
+				if(i==0 || i==n-1 || j==0 || j==n-1)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+				
+				Thread.sleep(50);
+			}
+			
+			
+			System.out.println();
+		}
+	}
+}
